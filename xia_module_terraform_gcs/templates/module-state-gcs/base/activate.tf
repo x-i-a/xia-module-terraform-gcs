84050b1d02e10259bfe556c9d5d31e45c8041f24
X-I-A <develop@x-i-a.com>
2024-04-08 20:46:47 +0200

    feat: Separate Terraform State Part

--- xia_module_terraform_gcs/templates/module-state-gcs/base/activate.tf
+++ xia_module_terraform_gcs/templates/module-state-gcs/base/activate.tf
@@ -3,7 +3,7 @@ module "activate_module_state_gcs" {
 
   landscape_file = var.landscape_file
   applications_file = var.applications_file
-  modules_file = var.modules_file
+  project_file = var.project_file
   github_provider_sa_dict = module.gcp_module_project.github_provider_sa
 
   depends_on = [module.gcp_module_project]
