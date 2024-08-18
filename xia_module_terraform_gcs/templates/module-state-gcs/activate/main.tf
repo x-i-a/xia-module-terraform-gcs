ca88bce893914db663c204422fab511073a9888e
X-I-A <develop@x-i-a.com>
2024-08-18 16:54:48 +0200

    feat: Removing unused parameters

--- xia_module_terraform_gcs/templates/module-state-gcs/activate/main.tf
+++ xia_module_terraform_gcs/templates/module-state-gcs/activate/main.tf
@@ -7,6 +7,7 @@ terraform {
 }
 
 locals {
+  project = yamldecode(file(var.project_file))
   landscape = yamldecode(file(var.landscape_file))
   applications = yamldecode(file(var.applications_file))
   settings = lookup(local.landscape, "settings", {})
@@ -14,6 +15,9 @@ locals {
   realm_name = local.settings["realm_name"]
   foundation_name = local.settings["foundation_name"]
   tf_bucket_name = lookup(local.settings, "cosmos_name")
+  folder_id = lookup(local.project, "folder_id", null)
+  project_prefix = local.project["project_prefix"]
+  billing_account = local.project["billing_account"]
   environment_dict = local.landscape["environments"]
   activated_apps = lookup(lookup(local.landscape["modules"], "gcp-module-project", {}), "applications", [])
 }
--- xia_module_terraform_gcs/templates/module-state-gcs/activate/variables.tf
+++ xia_module_terraform_gcs/templates/module-state-gcs/activate/variables.tf
@@ -1,3 +1,8 @@
+variable "project_file" {
+  type = string
+  description = "Project config file"
+}
+
 variable "landscape_file" {
   type = string
   description = "Landscape file"
