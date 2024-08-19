4f334c45635aeba8e54324e7c2a2734f87ed5daa
X-I-A <develop@x-i-a.com>
2024-08-19 22:59:59 +0200

    feat: cosmos level rework

--- xia_module_terraform_gcs/templates/module-state-gcs/base/activate.tf
+++ xia_module_terraform_gcs/templates/module-state-gcs/base/activate.tf
@@ -1,16 +1,7 @@
 module "activate_module_state_gcs" {
   source = "../../modules/activate-module-state-gcs"
 
-  config_file = "../../../config/core/tfstate.yaml"
   landscape = local.landscape
-  applications = local.applications
-  modules = local.modules
-  environment_dict = local.environment_dict
-  app_env_config = local.app_env_config
-  module_app_to_activate = local.module_app_to_activate
-
-  gcp_projects = module.gcp_module_project.gcp_projects
-  github_provider_sa_dict = module.gcp_module_project.github_provider_sa
-  depends_on = [module.gcp_module_project]
+  modules   = local.modules
 }
 
