1e93274ff152f65eeec7767641aca26c2d96e952
X-I-A <develop@x-i-a.com>
2024-08-21 21:54:44 +0200

    feat: cosmos level rework

--- xia_module_terraform_gcs/__init__.py
+++ xia_module_terraform_gcs/__init__.py
@@ -1,14 +1,17 @@
 from xia_module_terraform_gcs.gcs import GcsState
 from xia_module_terraform_gcs.foundation_gcs import FoundationGcsState
+from xia_module_terraform_gcs.application_gcs import ApplicationGcsState
 
 modules = {
     "module-state-gcs": "GcsState",
     "module-foundation-state-gcs": "FoundationGcsState",
+    "module-application-state-gcs": "ApplicationGcsState",
 }
 
 __all__ = [
     "GcsState",
-    "FoundationGcsState"
+    "FoundationGcsState",
+    "ApplicationGcsState"
 ]
 
 __version__ = "0.0.5"
---
+++ xia_module_terraform_gcs/application_gcs.py
@@ -0,0 +1,6 @@
+import os
+from xia_module import Module
+
+
+class ApplicationGcsState(Module):
+    module_name = "module-application-state-gcs"
