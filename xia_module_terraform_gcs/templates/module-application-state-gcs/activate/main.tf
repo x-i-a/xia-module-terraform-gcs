3d4e3c0467e0485adefec86b33bd8c93c985a95f
X-I-A <develop@x-i-a.com>
2024-08-21 22:03:08 +0200

    feat: cosmos level rework

--- xia_module_terraform_gcs/templates/module-application-state-gcs/activate/main.tf
+++ xia_module_terraform_gcs/templates/module-application-state-gcs/activate/main.tf
@@ -8,4 +8,14 @@ terraform {
 
 locals {
   module_name = coalesce(var.module_name, substr(basename(path.module), 9, length(basename(path.module)) - 9))
+  tfstate_config = yamldecode(file(var.config_file))
+  tf_bucket_name = local.tfstate_config["tf_bucket"]
+}
+
+
+resource "google_storage_bucket_iam_member" "tfstate_bucket_admin" {
+  for_each = var.foundations
+  bucket = local.tf_bucket_name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${var.foundation_admin_sa[each.key].email}"
 }
