a0644d91a233c9f62065c2e8521e83cad8ea7b95
X-I-A <develop@x-i-a.com>
2024-08-21 21:56:40 +0200

    feat: cosmos level rework

--- xia_module_terraform_gcs/templates/module-application-state-gcs/base/activate.tf
+++ xia_module_terraform_gcs/templates/module-application-state-gcs/base/activate.tf
@@ -3,5 +3,9 @@ module "activate_module_application_state_gcs" {
 
   landscape = local.landscape
   modules   = local.modules
+  foundations = local.foundations
+
+  foundation_admin_sa = module.gcp_module_organization.foundation_admin_sa
+  depends_on = [module.gcp_module_organization]
 }
 
