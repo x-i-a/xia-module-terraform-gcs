7cb63ff6c00b95c70262bd8c8551ce215f71e005
X-I-A <develop@x-i-a.com>
2024-09-08 17:35:08 +0200

    feat: adding cosmos backend

--- xia_module_terraform_gcs/__init__.py
+++ xia_module_terraform_gcs/__init__.py
@@ -1,9 +1,11 @@
+from xia_module_terraform_gcs.cosmos_backend_gcs import CosmosGcsBackend
 from xia_module_terraform_gcs.foundation_gcs import FoundationGcsState
 from xia_module_terraform_gcs.foundation_backend_gcs import FoundationGcsBackend
 from xia_module_terraform_gcs.application_gcs import ApplicationGcsState
 from xia_module_terraform_gcs.application_backend_gcs import ApplicationGcsBackend
 
 modules = {
+    "module-cosmos-backend-gcs": "CosmosGcsBackend",
     "module-foundation-state-gcs": "FoundationGcsState",
     "module-foundation-backend-gcs": "FoundationGcsBackend",
     "module-application-state-gcs": "ApplicationGcsState",
@@ -11,10 +13,11 @@ modules = {
 }
 
 __all__ = [
+    "CosmosGcsBackend",
     "FoundationGcsState",
     "FoundationGcsBackend",
     "ApplicationGcsState",
     "ApplicationGcsBackend"
 ]
 
-__version__ = "0.0.20"
+__version__ = "0.0.21"
---
+++ xia_module_terraform_gcs/cosmos_backend_gcs.py
@@ -0,0 +1,6 @@
+import os
+from xia_module import Module
+
+
+class CosmosGcsBackend(Module):
+    module_name = "module-cosmos-backend-gcs"
